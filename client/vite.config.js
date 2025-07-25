078835519d5af532a5d7b11912f7402d650c08a7
BUTURUM <maxvolkov502@gmail.com>
2025-07-25 17:47:35 +0100

    feat: add server directory and vite proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,5 +2,16 @@ import { defineConfig } from "vite";
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        ws: true
+      }
+    }
+  },
+  build: {
+    outDir: '../server/static'
+  },
   plugins: [vue()]
 });
