c7746660d04e75124a393fec4b8ce632bb7f5787
BUTURUM <maxvolkov502@gmail.com>
2025-07-25 21:36:15 +0100

    feat: create main page

---
+++ client/src/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="header">
+    <h1 class="title">DarkMysteries</h1>
+    <ul class="links">
+      <li>How to Play</li>
+      <li>About Us</li>
+    </ul>
+  </header>
+</template>
+<style scoped>
+  .header{
+    display: flex;
+    padding: 20px 30px;
+    align-items: center;
+  }
+  .title{
+    flex: 1;
+  }
+  .links{
+    flex-basis: auto;
+    flex-shrink: 0;
+  }
+  .links{
+    display: flex;
+    justify-content: center;
+    gap: 25px;
+  }
+</style>
---
+++ client/src/Story.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="story">
+    <div class="icon-box">
+      <span class="icon">{{ emoji }}</span>
+    </div>
+    <h3 class="title">{{ title }}</h3>
+  </div>
+</template>
+<script setup>
+  const { title, emoji } = defineProps(['title', 'emoji']);
+</script>
+<style scoped>
+  .story{
+    display: flex;
+    flex-direction: column;
+
+    width: 200px;
+    height: 200px;
+
+    box-sizing: border-box;
+    padding: 20px;
+
+    transition: background-color 100ms;
+    border: var(--primary-color) dashed 5px;
+  }
+  .story:hover{
+    background-color: var(--primary-color);
+  }
+  .icon-box{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    flex: 1;
+  }
+  .icon{
+    font-size: 64px;
+    font-family: 'Noto Emoji';
+    user-select: none;
+    color: red;
+  }
+  .title{
+    text-align: center;
+  }
+</style>
