ce9d7ec9f620c866aa5fa911aefc2d5b7def2630
BUTURUM <maxvolkov502@gmail.com>
2025-07-26 18:04:54 +0100

    fix: /story/:id now actually works

--- server/api/api.py
+++ server/api/api.py
@@ -73,8 +73,8 @@ def get_story_titles():
     return all_stories.get_all_stories()
 
 
-# return a specific story
-@app.get("/api/story")
+#return a specific story
+@app.get("/api/story/{id}")
 def get_story(id: int):
     story = all_stories.find_story("id", id)
     if story == None:
